9dce35aeba30188f2ecbed6921a97a723d65d277
Mateo Valdesolo <mateovalde@gmail.com>
2021-05-31 20:21:41 -0300

    Implementacion de Nodo de ArbolHash

---
+++ src/conjuntistas/Nodo.java
@@ -0,0 +1,28 @@
+package conjuntistas;
+
+public class Nodo {
+    
+    private Object elem;
+    private Nodo enlace;
+
+    public Nodo(Object elem, Nodo enlace){
+        this.elem = elem;
+        this.enlace = enlace;
+    }
+
+    public void setElem(Object elem){
+        this.elem = elem;
+    }
+
+    public void setEnlace(Nodo enlace){
+        this.enlace = enlace;
+    }
+
+    public Object getElem(){
+        return this.elem;
+    }
+
+    public Nodo getNodo(){
+        return this.enlace;
+    }
+}
