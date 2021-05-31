f2e2955e40ed59c630ecc9ba497a29b1b52dfaff
Mateo Valdesolo <mateovalde@gmail.com>
2021-05-31 20:21:06 -0300

    Comienzo implementacion ArbolAVL

---
+++ src/conjuntistas/ArbolAVL.java
@@ -0,0 +1,201 @@
+package conjuntistas;
+
+import lineales.dinamicas.Lista;
+
+public class ArbolAVL {
+
+    private NodoAVL raiz;
+
+    public ArbolAVL() {
+        this.raiz = null;
+    }
+
+    public boolean pertenece(Comparable elem) {
+        /*
+         * Devuelve verdadero si el elemento recibido por parámetro está en el árbol y
+         * falso en caso contrario.
+         */
+        boolean exito = false;
+        if (!esVacio()) {
+            exito = perteneceAux(this.raiz, elem);
+        }
+        return exito;
+    }
+
+    private boolean perteneceAux(NodoAVL nodo, Comparable elem) {
+        boolean exito = false;
+        int res = nodo.getElem().compareTo(elem);
+        if (res == 0) {
+            exito = true;
+        } else {
+            if (res < 0) {
+                if (nodo.getDerecho() != null) {
+                    exito = perteneceAux(nodo.getDerecho(), elem);
+                }
+            } else {
+                if (nodo.getIzquierdo() != null) {
+                    exito = perteneceAux(nodo.getIzquierdo(), elem);
+                }
+            }
+        }
+        return exito;
+    }
+
+    public boolean insertar(Comparable elem) {
+
+    }
+
+    public boolean eliminar(Comparable elem) {
+
+    }
+
+    public Lista listar() {
+        /*
+         * Recorre el árbol completo y devuelve una lista ordenada con los elementos que
+         * se encuentran almacenados en él.
+         */
+        Lista ret = new Lista();
+        listarAux(this.raiz, ret);
+        return ret;
+    }
+
+    private void listarAux(NodoAVL nodo, Lista lis) {
+        if (nodo != null) {
+            listarAux(nodo.getIzquierdo(), lis);
+            lis.insertar(nodo.getElem(), lis.longitud() + 1);
+            listarAux(nodo.getDerecho(), lis);
+        }
+    }
+
+    public Lista listarRango(Comparable elemMinimo, Comparable elemMaximo) {
+        /*
+         * Recorre parte del árbol (sólo lo necesario) y devuelve una lista ordenada con
+         * los elementos que se encuentran en el intervalo [elemMinimo, elemMaximo].
+         */
+        Lista ret = new Lista();
+        listarRangoAux(this.raiz, elemMinimo, elemMaximo, ret);
+        return ret;
+    }
+
+    private void listarRangoAux(NodoAVL nodo, Comparable minim, Comparable maxim, Lista lis) {
+        if (nodo != null) {
+            if (nodo.getElem().compareTo(minim) > 0) {
+                listarRangoAux(nodo.getIzquierdo(), minim, maxim, lis);
+            }
+            if (nodo.getElem().compareTo(minim) >= 0 && nodo.getElem().compareTo(maxim) <= 0) {
+                lis.insertar(nodo.getElem(), lis.longitud() + 1);
+            }
+            if (nodo.getElem().compareTo(maxim) < 0) {
+                listarRangoAux(nodo.getDerecho(), minim, maxim, lis);
+            }
+        }
+    }
+
+    public Comparable minimoElem() {
+        /*
+         * Recorre la rama correspondiente y devuelve el elemento más pequeño almacenado
+         * en el árbol.
+         */
+        NodoAVL nodo = this.raiz;
+        while (nodo.getIzquierdo() != null) {
+            nodo = nodo.getIzquierdo();
+        }
+        return nodo.getElem();
+    }
+
+    public Comparable maximoElem() {
+        /*
+         * Recorre la rama correspondiente y devuelve el elemento más grande almacenado
+         * en el árbol.
+         */
+        NodoAVL nodo = this.raiz;
+        while (nodo.getDerecho() != null) {
+            nodo = nodo.getDerecho();
+        }
+        return nodo.getElem();
+    }
+
+    public boolean esVacio() {
+        /*
+         * Devuelve falso si hay al menos un elemento en el árbol y verdadero en caso
+         * contrario.
+         */
+        return this.raiz == null;
+    }
+
+    public void vaciar() {
+        /*
+         * Vacia la estructura.
+         */
+        this.raiz = null;
+    }
+
+    public ArbolAVL clone() {
+        /*
+         * Genera y devuelve un árbol binario que es equivalente (igual estructura y
+         * contenido de los nodos) que el árbol original.
+         */
+        ArbolAVL clon = new ArbolAVL();
+        if (!esVacio()) {
+            NodoAVL nod = new NodoAVL(this.raiz.getElem(), null, null);
+            clon.raiz = nod;
+            cloneAux(nod, this.raiz);
+        }
+        return clon;
+    }
+
+    private void cloneAux(NodoAVL aux, NodoAVL nodo) {
+        if (nodo != null) {
+            if (nodo.getIzquierdo() != null) {
+                aux.setIzquierdo(new NodoAVL(nodo.getIzquierdo().getElem(), null, null));
+                cloneAux(aux.getIzquierdo(), nodo.getIzquierdo());
+            }
+            if (nodo.getDerecho() != null) {
+                aux.setDerecho(new NodoAVL(nodo.getDerecho().getElem(), null, null));
+                cloneAux(aux.getDerecho(), nodo.getDerecho());
+            }
+        }
+    }
+
+    public String toString() {
+        /*
+         * Genera y devuelve una cadena de caracteres que indica cuál es la raíz del
+         * árbol y quienes son los hijos de cada nodo.
+         */
+        String ret = "[]";
+        if (!esVacio()) {
+            ret = toStringAux(this.raiz, "");
+        }
+        return ret;
+    }
+
+    private String toStringAux(NodoAVL nodo, String str) {
+
+        if (nodo != null) {
+            NodoAVL izq = nodo.getIzquierdo(), der = nodo.getDerecho();
+            str += "Nodo: " + nodo.getElem();
+            if (izq != null && der != null) {
+                str += " HI: " + izq.getElem();
+                str += " HD: " + der.getElem() + "\n";
+            } else {
+                if (izq == null && der == null) {
+                    str += " HI: - ";
+                    str += " HD: - " + "\n";
+                } else {
+                    if (izq == null) {
+                        str += " HI: -";
+                        str += " HD: - " + der.getElem() + "\n";
+                    } else {
+                        if (der == null) {
+                            str += " HI: " + izq.getElem();
+                            str += " HD: - " + "\n";
+                        }
+                    }
+                }
+            }
+            str = toStringAux(izq, str);
+            str = toStringAux(der, str);
+        }
+        return str;
+    }
+}
