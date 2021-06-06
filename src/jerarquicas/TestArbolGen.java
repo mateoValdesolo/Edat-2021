73a83c2bff417f9591b00c4a4e8473e10ca7eec9
Mateo Valdesolo <mateovalde@gmail.com>
2021-06-06 12:42:15 -0300

    Cambios

--- src/jerarquicas/TestArbolGen.java
+++ src/jerarquicas/TestArbolGen.java
@@ -1,11 +1,11 @@
 package jerarquicas;
-
+import lineales.dinamicas.Lista;
 public class TestArbolGen {
 
     public static void main(String[] args) {
         ArbolGen arb1 = new ArbolGen();
         ArbolGen arb2 = new ArbolGen();
-        System.out.println("Arbol Vacio: "+arb1.toString());
+        /*System.out.println("Arbol Vacio: "+arb1.toString());
         System.out.println("EsVacio: "+arb1.esVacio());
         System.out.println("Pertenece: "+arb1.pertenece('D'));
         System.out.println("Padre: "+arb1.padre('C'));
@@ -39,7 +39,32 @@ public class TestArbolGen {
         System.out.println("Posorden: "+arb1.listarPosorden());
         System.out.println("PorNiveles: "+arb1.listarPorNiveles());
         System.out.println("Preorden: "+arb1.listarPreorden());
-        System.out.println("Inserto en pos inexistente: "+arb1.insertar('A', 'N'));
+        System.out.println("Inserto en pos inexistente: "+arb1.insertar('A', 'N'));*/
+        ArbolGen arb3 = new ArbolGen();
+        arb3.insertar('A', 'A');
+        arb3.insertar('B', 'A');
+        arb3.insertar('C', 'A');
+        arb3.insertar('D', 'B');
+        arb3.insertar('E', 'B');
+        arb3.insertar('F', 'E');
+        arb3.insertar('G', 'C');
+        arb3.insertar('H', 'C');
+        arb3.insertar('I', 'C');
+        arb3.insertar('J', 'H');
+        System.out.println(arb3.toString());
+        System.out.println(arb3.listarEntreNiveles(2, 3));
+        /*Lista lis2 = new Lista(), lis = new Lista();
+        lis.insertar('A', 1);
+        lis.insertar('C', 2);
+        lis.insertar('I', 3);
+        lis2.insertar('A', 1);
+        lis2.insertar('B', 2);
+        lis2.insertar('E', 3);
+        lis2.insertar('F', 4);
+        System.out.println("<A,B,E>: "+arb3.verificarCamino(lis));
+        System.out.println("<A,B,E,F>: "+arb3.verificarCamino(lis2));*/
+
+
     }
 
 }
